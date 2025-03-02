eff5866c3eb4703919fbab043df5d467a7d01a24
Keith Thomson <camelbyte@proton.me>
2025-03-02 11:03:03 -0500

    Create mdb-handler.go

---
+++ mdb-handler.go
@@ -0,0 +1,19 @@
+import (
+  "fmt"
+  "net/http"
+)
+
+
+type mdbHandler struct {
+  user: "keith-mdb",
+  port: "whatever port MDB is",
+  Ip: "IP address"
+  Id: "authentication",
+  }
+
+
+func(m *mdbHandler) (s string) {
+    
+  }
+
+
